70153a7adec4cf9869bc13ae2444761f8a0072ae
Simon he <13917107460@163.com>
2022-08-02 22:45:48 +0800

    chore: lint

--- test/vitest.test.ts
+++ test/vitest.test.ts
@@ -1,25 +1,25 @@
-import { describe, it, expect, test } from 'vitest'
+import { describe, expect, it, test } from 'vitest'
 describe.skip('hi', async () => {
   it('should works', () => {
     const a = 2
-    expect(a).toEqual(2);
+    expect(a).toEqual(2)
   })
   it('should works', () => {
     const a = 2
-    expect(a).toMatchInlineSnapshot();
+    expect(a).toMatchInlineSnapshot()
   })
   test('should works', () => {
     const a = 2
-    expect(a).toEqual(2);
+    expect(a).toEqual(2)
   })
 })
 describe('hi', async () => {
   it('should works', () => {
     const a = 2
-    expect(a).toEqual(2);
+    expect(a).toEqual(2)
   })
   it('should works', () => {
     const a = 2
-    expect(a).toMatchInlineSnapshot();
+    expect(a).toMatchInlineSnapshot()
   })
 })
